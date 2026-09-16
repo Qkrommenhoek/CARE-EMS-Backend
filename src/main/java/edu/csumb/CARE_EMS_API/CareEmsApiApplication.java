7d97215874b6d1fea66aa88a65aa50f863a615a4
qkrommenhoek <qkrommenhoek@gmail.com>
2026-09-16 09:55:37 -0700

    Spring Initializr

---
+++ src/main/java/edu/csumb/CARE_EMS_API/CareEmsApiApplication.java
@@ -0,0 +1,13 @@
+package edu.csumb.CARE_EMS_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareEmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareEmsApiApplication.class, args);
+	}
+
+}
